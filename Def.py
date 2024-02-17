b418230ec0fee0e7e8ddf6b8abdf654f8a9119d8
MostafaDev <32361640+sudo69@users.noreply.github.com>
2024-02-17 22:29:27 +0200

    Add files via upload

---
+++ Def.py
@@ -0,0 +1,13 @@
+def pizza(pizza_content):
+    print(f"pizza content is {pizza_content}")
+
+
+pizza("cheese")
+
+
+def the_rest_number(num):
+    return num * 2
+
+
+returned_numbers = the_rest_number(10)
+print(returned_numbers)
